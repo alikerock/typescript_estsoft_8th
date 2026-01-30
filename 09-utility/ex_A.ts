f544a4a58b89cabba9cb69ae5f973445108d2ad9
alikerock <alikerock@gmail.com>
2026-01-30 13:19:15 +0900

    type utility

--- 09-utility/ex_A.ts
+++ 09-utility/ex_A.ts
@@ -5,10 +5,10 @@ const a2: UserDraftA = {};                 // OK
 
 type UserStrictA = Required<User>;
 /* 아래 객체를 UserStrictA에 맞게 완성해 보자 */
-const a3 /* : UserStrictA */ = {
+const a3: UserStrictA = {
   id: 1,
   name: 'Kim',
   role: 'member',
-  //   <- 추가 작성 email 속성의 값으로 'kim@example.com'
+  email:'kim@abc.com'
 };
 
